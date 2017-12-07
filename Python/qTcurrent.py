d60a0b138d235c449b77754e154cfeb9f12602b1
JQubit <30702589+JQubit@users.noreply.github.com>
2017-12-07 00:47:20 -0300

    qTcurrent.py : Plotting and Integration of F-D functions
    Fermi-Dirac (F-D) distribution functions and differences of them are plotted as well as the integral over the difference function.
    
    There is a RuntimeWarning (that occurs when calculating the F-D function) that must be solved.

--- Python/qTcurrent.py
+++ Python/qTcurrent.py
@@ -15,27 +15,66 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy import units as u
 from astropy import constants as const
-from scipy import integrate as sc_int
+from scipy import integrate
+from decimal import Decimal
 
 
+def fermi_dirac_fn(energy, temperature, chemical_potential):
+    """
+    Fermi-Dirac distribution function. Use (as inputs) floats with units provided by the 'astropy' module to keep
+    track of the correct units of the result.
+    :param energy: independent variable, the energy (E) as a float number.
+    :param temperature: fixed value of temperature (T) of the system, as a float number.
+    :param chemical_potential: chemical potential (mu) that characterizes the system at T, as a float number.
+    :return: value (float) of the Fermi-Dirac distribution function evaluated in E with the particular mu.
+    """
+    return 1. / (np.exp(-(energy - chemical_potential) / (const.k_B * temperature)) + 1)
+
+
+# Parameters:
+temperature_float = 300. * u.K
+chemical_potential_float = 5.53 * u.eV  # aprox. by Fermi energy (of Au)
+energy_initial_float = 0. * u.eV
+energy_final_float = 10 * u.eV
+energy_step_float = 0.01 * u.eV
+energy_steps_number_float = (energy_final_float - energy_initial_float) / energy_step_float
+energy_bias_float = 4. * u.eV
+
+energy_array = np.linspace(energy_initial_float.value, energy_final_float.value + energy_step_float.value,
+                           energy_steps_number_float.value) * energy_step_float.unit
+
+# Calculate:
+fermiDiracLeft_array = fermi_dirac_fn(energy_array, temperature_float, chemical_potential_float)
+fermiDiracRight_array = fermi_dirac_fn(energy_array + energy_bias_float, temperature_float, chemical_potential_float)
+fermiDiracDiff_array = fermiDiracLeft_array - fermiDiracRight_array
+
+integral_value_float = integrate.trapz(fermiDiracDiff_array, energy_array)
+
+# Plot:
 plt.figure(1)
-plt.plot(a2_grid[0, 0, :], prob_quad_a2, '-', color='orange', label='Prob. Marginal')
-plt.title('Probabilidad Marginal de $a_2$')
-plt.xlabel('$a_2$', fontsize=12)
-plt.ylabel('$P_{a_2}^M$', fontsize=12)
-plt.axvline(x=mean_a2, ls='--', color='r', label='Media = ' + '%.3E' % Decimal(str(mean_a2)))
-plt.axvline(x=mean_a2 + std_a2, ls='-.', color='g')
-plt.axvline(x=mean_a2 - std_a2, ls='-.', color='g')
-plt.fill_between(a2_grid[0, 0, :], prob_quad_a2, where=credibility_region_1sigma_a2, color=(204/255, 1, 153/255))
+plt.plot(energy_array, fermiDiracDiff_array, '-', color='orange', label='F-D diff.')
+plt.plot(energy_array, fermiDiracLeft_array, '--', color='r', label='F-D Left')
+plt.plot(energy_array, fermiDiracRight_array, '--', color='m', label='F-D Right')
+plt.title('Fermi-Dirac functions')
+plt.xlabel('Energy [eV]', fontsize=12)
+plt.ylabel('Probability Distribution Density', fontsize=12)
+plt.axvline(x=chemical_potential_float.value, ls=':', color='g', label='Chem. Pot. = ' +
+                                                                  '%.3E' % Decimal(str(chemical_potential_float.value)))
+plt.axvline(x=(chemical_potential_float - energy_bias_float).value,
+            ls=':', color='b', label='Chem. Pot. - Bias Energy = ' +
+                                      '%.3E' % Decimal(str((chemical_potential_float - energy_bias_float).value)))
+#plt.axvline(x=(chemical_potential_float + const.k_B * temperature_float).value, ls='-.', color='g')
+#plt.axvline(x=(chemical_potential_float - const.k_B * temperature_float).value, ls='-.', color='g')
+plt.fill_between(energy_array.value, fermiDiracDiff_array.value, color=(1, 1, 224/255, 0.3),
+                 label='Integral = ' + '%.3E' % Decimal(str(integral_value_float.value)))
 plt.legend()
 plt.grid(linestyle='--')
 
 
-# Guardar gráficos como archivos .eps (vectorial) y .png (mapa de bits):
-# (Esto es para agregar al informe posteriormente (el .eps) y para ver sin utilizar python cuando se desee (.png))
+# Save figures as .eps (vectorial) and .png (bits map) files:
 plt.figure(1)
-plt.savefig('P1_plot_datos.eps')
-plt.savefig('P1_plot_datos.png')
+plt.savefig('fermiDiracDiff.eps')
+plt.savefig('fermiDiracDiff.png')
 
-# Ver gráficos con IPython:
+# See figures with IPython:
 plt.show()
