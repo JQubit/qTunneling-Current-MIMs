e89ae21edbfea514d982cae9bd36cb50dd9b8636
JQubit <30702589+JQubit@users.noreply.github.com>
2017-12-08 19:28:44 -0300

    qTcurrent.py : Integration range optimized
    The integration range has been optimized to not go beyond the minimum limits that guarantee a good calculation (for now, this has been "arbitrarily" defined by me as 10 * k_B * T beyond the concavity change points (where the energy equals the "effective" chemical potentials))

--- Python/qTcurrent.py
+++ Python/qTcurrent.py
@@ -28,17 +28,26 @@ def fermi_dirac_fn(energy, temperature, chemical_potential):
     :param chemical_potential: chemical potential (mu) that characterizes the system at T, as a float number.
     :return: value (float) of the Fermi-Dirac distribution function evaluated in E with the particular mu.
     """
-    return 1. / (np.exp(-(energy - chemical_potential) / ((const.k_B * temperature)).to(u.eV)) + 1)
+    return 1. / (np.exp(-(energy - chemical_potential) / (const.k_B * temperature).to(u.eV)) + 1)
 
 
 # Parameters:
 temperature_float = 300. * u.K
-chemical_potential_float = 5.53 * u.eV  # aprox. by Fermi energy (of Au)
+chemical_potential_float = 5.53 * u.eV  # approx. by Fermi energy (of Au)
+energy_bias_float = 4. * u.eV
 energy_initial_float = 0. * u.eV
 energy_final_float = 10. * u.eV
+integration_minimum_range_floats_tuple = (chemical_potential_float - 10. * (const.k_B * temperature_float).to(u.eV)
+                                          - energy_bias_float,
+                                          chemical_potential_float + 10. * (const.k_B * temperature_float).to(u.eV))
+# Reduce integration range to the minimum acceptable (defined above), when the integration limits go beyond the
+# minimum integration range limits:
+if energy_initial_float < integration_minimum_range_floats_tuple[0]:
+    energy_initial_float = integration_minimum_range_floats_tuple[0]
+if energy_final_float > integration_minimum_range_floats_tuple[1]:
+    energy_final_float = integration_minimum_range_floats_tuple[1]
 energy_step_float = 0.01 * u.eV
 energy_steps_number_float = (energy_final_float - energy_initial_float) / energy_step_float
-energy_bias_float = 4. * u.eV
 
 energy_array = np.linspace(energy_initial_float.value, energy_final_float.value + energy_step_float.value,
                            energy_steps_number_float.value) * energy_step_float.unit
@@ -52,9 +61,9 @@ integral_value_float = integrate.trapz(fermiDiracDiff_array, energy_array)
 
 # Plot:
 plt.figure(1)
-plt.plot(energy_array, fermiDiracDiff_array, '-', color='orange', label='$n_{FD}^L(E) - n_{FD}^R(E+U_{bias})$')
+plt.plot(energy_array, fermiDiracDiff_array, '-', color='orange', label='$n_{FD}^L(E) - n_{FD}^R(E)$')
 plt.plot(energy_array, fermiDiracLeft_array, '--', color='r', label='$n_{FD}^L(E)$')
-plt.plot(energy_array, fermiDiracRight_array, '--', color='m', label='$n_{FD}^R(E+U_{bias})$')
+plt.plot(energy_array, fermiDiracRight_array, '--', color='m', label='$n_{FD}^R(E) \equiv n_{FD}^L(E+U_{bias})$')
 plt.title('Fermi-Dirac functions')
 plt.xlabel('Energy [eV]', fontsize=12)
 plt.ylabel('Probability Distribution Density', fontsize=12)
